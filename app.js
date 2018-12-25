0b8f0738f1fabda071f15609f15a347b3b4f8ea3
Heungsoo Kim <hkim450@gmail.com>
2018-12-24 22:46:50 -0600

    checkpoint update

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ function onReady() {
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  addToDoForm,addEventListener('submit', event => {
-    event.preventDefult();
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
 
     // get the text
     let title = newToDoText.value;
 
     // create a new li
-    let newLi = document.createElement('Li');
+    let newLi = document.createElement('li');
 
     // create a new input
     let checkbox = document.createElement('input');
@@ -27,8 +27,8 @@ function onReady() {
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
-    // empty the input
-    newToDoText.value = '';
+    //empty the input
+   newToDoText.value = '';
   });
 }
 
