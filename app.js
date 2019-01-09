2b4b857d97b905393d59e03b1f1aad43589d4fa6
Heungsoo Kim <hkim450@gmail.com>
2019-01-08 21:44:02 -0600

    Assignment events part 2 updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function onReady() {
-  let toDoId = 0;
-  let toDos =[];
+  let toDoId = 0
+  let toDos = []
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
@@ -25,26 +25,27 @@ function onReady() {
 
     toDos.forEach(function(toDo) {
       const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
+
       const deleteBtn = document.createElement('button');
       deleteBtn.textContent = "Delete";
 
-      newLi.textContent = toDo.title;
-
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-      newLi.appendChild(deleteBtn);
-
-      deleteBtn.addEventListener('click', function deleteToDo(element, index){
-        element.parentElement.removeChild(element);
-
-        toDos = toDos.filter((todo => {
-          return todo.id != index;
-        }));
+      deleteBtn.addEventListener('click', function(event){
+        deleteToDo(newLi, toDo.id);
       });
 
+      newLi.textContent = toDo.title;
+      newLi.appendChild(deleteBtn);
+      toDoList.appendChild(newLi);
+
     });
+  }
+
+  function deleteToDo(element, index) {
+    element.parentElement.removeChild(element);
+
+    toDos = toDos.filter((todo) => {
+      return todo.id != index;
+    })
   }
 
   addToDoForm.addEventListener('submit', event => {
